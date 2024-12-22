c8ea21142324664c906eb3fb349ae12f730bc189
tengku <22001741@siswa.um.edu.my>
2024-12-22 19:24:10 +0800

    first commit, still got error but donno how to fix it

---
+++ app/src/main/java/com/example/educationpeacemy/ListData.java
@@ -0,0 +1,15 @@
+package com.example.educationpeacemy;
+
+public class ListData {
+    int name;
+    int image;
+    int description, symptoms, treatment;
+
+    public ListData(int name, int image, int description, int symptoms, int treatment) {
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.symptoms = symptoms;
+        this.treatment = treatment;
+    }
+}
